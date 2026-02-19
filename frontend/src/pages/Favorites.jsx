c14ea869ca5a662adbd3cc5225a26aa565becf4b
gurshivdabb <dabbgurshiv@gmail.com>
2026-02-18 20:20:13 -0600

    Create Favorites.jsx (with contents)

---
+++ frontend/src/pages/Favorites.jsx
@@ -0,0 +1,8 @@
+function Favorites() {
+    return <div className="favorites-empty">
+        <h2>No Favorite Games Yet</h2>
+        <p>Start adding games to your favorites!</p>
+    </div>
+}
+
+export default Favorites;
